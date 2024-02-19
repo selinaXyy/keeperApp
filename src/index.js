7fea1488dc3f6b144971b2652d0d7b344962bfef
Selina Xue <selinaxue@selinas.home>
2024-02-18 18:26:27 -0800

    Update index.js, styles.css. Create Header.jsx, Note.jsx, Footer.jsx, App.jsx.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(<h1>HELLO WORLD</h1>,document.getElementById("root"));
+ReactDOM.render(
+  <App />
+  ,document.getElementById("root"));
